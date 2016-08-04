7ae0990d91fc3b4f366b72bb2db62754bdd704cf
SimonBaumannPro <simonbaumann20@gmail.com>
2016-08-04 15:30:18 +0000

    code simplification with functions

--- src/backup script.js
+++ src/backup script.js
@@ -21,7 +21,6 @@ var drawspace = $(".drawspace"),
     legobase = new Webcom(webcom_url),
     domain="brick";
 
-
 $(window).on("load", function (){
 
   $(document).foundation();
@@ -29,46 +28,50 @@ $(window).on("load", function (){
   // Initialisation globale du contexte
   globalInit();
 
+  // = 1 si mobile, 0 sinon
+  smartphone = detectDevice();
+
+
   // Gestion des évenements tactiles (pan & zoom)
   hammerIt(drawspace[0]);
 
-  // Gestion de l'authentification
-  if (noAuth) {
-    authData={uid: "anonymous", provider: "none", none: {displayName: "anonymous"}};
-  }
+  /* ------------ Gestion du tactile ------------ */
 
-  // drawspace scale from 2 to 1
   btn_dezoom.onclick = function(){
     dezoom();
   };
   
-  $(function() {
-    $("body").swipe( {
-      swipeStatus:function(event, phase, direction, distance , duration , fingerCount) {
+  (function() {
+  $("body").swipe( {
+    swipeStatus:function(event, phase, direction, distance , duration , fingerCount) {
         if(phase === $.fn.swipe.phases.PHASE_END || phase === $.fn.swipe.phases.PHASE_CANCEL) {
           //The handlers below fire after the status, 
           // so we can change the text here, and it will be replaced if the handlers below fire
           $(this).find('#swipe_text').text("No swipe was made");
-        }
+         }
       },
-      swipeLeft:function(event, direction, distance, duration, fingerCount) {
-        if (fingerCount == 1 && duration < 250) {
-          offCanvas.foundation("open", offCanvas);
-        }        
-      },
-      swipeRight:function(event, direction, distance, duration, fingerCount) {
-        if (fingerCount == 1) {
-          offCanvas.foundation('close');
-        }
-      },
-      fingers:$.fn.swipe.fingers.ALL  
-    });
+    swipeLeft:function(event, direction, distance, duration, fingerCount) {
+      if (fingerCount == 1 && duration < 250) {
+        offCanvas.foundation("open", offCanvas);
+      }        
+    },
+    swipeRight:function(event, direction, distance, duration, fingerCount) {
+      if (fingerCount == 1) {
+        offCanvas.foundation('close');
+      }
+    },
+    fingers:$.fn.swipe.fingers.ALL  
   });
+  });
+
+
+
 
   /* Supprime toutes les briques du drawspace */
   eraseAll.click(function() {
     legobase.child(domain).remove();
   });
+
 
   // Callback sur changement d'une brique. Dans notre cas c'est juste la couleur qui change
   legobase.child(domain).on('child_changed', function(snapshot) {
@@ -88,7 +91,7 @@ $(window).on("load", function (){
     bricks[brick.x+"-"+brick.y]=brick_div;
     
     drawspace.append(brick_div);
-    $("#bricks_count").html(Object.keys(bricks).length);
+	  $("#bricks_count").html(Object.keys(bricks).length);
   }); 
 
   // Callback sur la suppression d'une brique
@@ -96,26 +99,88 @@ $(window).on("load", function (){
     var brick=snapshot.val();
     bricks[brick.x+"-"+brick.y].remove();
     delete bricks[brick.x+"-"+brick.y];
-    $("#bricks_count").html(Object.keys(bricks).length);
+	  $("#bricks_count").html(Object.keys(bricks).length);
   });
 
+  // Gestion de l'authentification
+  if (noAuth) {
+    $("#connect").hide();
+    $("#disconnect").hide();
+    authData={uid: "anonymous", provider: "none", none: {displayName: "anonymous"}};
+  } else {
+    legobase.resume(checkAuth);
+  }
 
-  drawspace.on('mousedown', function(e){
-    e.preventDefault();
+  // Ici se termine le code du "backend".
 
-    var handlers = {
+  /* gère la surbrillance d'une couleur active */
+  $(".colors .brickMenu").click(function(e) {
+    $(".colors .brickMenu").removeClass("active");
+    $(this).addClass("active");
+    color=$(this).attr('class').replace(/\s*(brick|active)\s*/g, '');
+  });
+  
+  /* empèche l'affichage des couleurs lors du déroulement de la liste si on est en mode "erase" */
+  $("#titleDT").click(function() {
+    if (mode == "erase") {
+      $(".ul-drawTools").attr("style", "height: 100px");
+      $(".drawTools-buttons-container").css("height", "100%");
+      $(".color-list").hide();
+    }
+  });
+
+  $(".button").click(function(e){
+    change_mode($(this).attr("id"))
+  });
+    
+  function change_mode(new_mode) {
+    mode=new_mode;
+    if (mode=="draw" || mode=="eraseAll") {
+      $(".ul-drawTools").attr("style", "height: 200px");
+      $(".drawTools-buttons-container").css("height", "50%");
+      $("#erase"+" :nth-child(1)").removeClass("fa-square").addClass("fa-square-o");
+      $("#draw"+" :nth-child(1)").removeClass("fa-square-o").addClass("fa-square");
+      $(".color-container").show();
+    }
+    else {
+      $(".ul-drawTools").attr("style", "height: 100px");
+      $(".drawTools-buttons-container").css("height", "100%");
+      $("#erase"+" :nth-child(1)").removeClass("fa-square-o").addClass("fa-square");
+      $("#draw"+" :nth-child(1)").removeClass("fa-square").addClass("fa-square-o");
+      $(".color-list").hide();      
+    }
+  }
+
+  $(".drawspace").on('mousedown', function(e){
+	  e.preventDefault();
+    var target = $( e.target );
+    var options_div=target.closest(".options");
+    var position = options_div.offset() || target.offset();
+    var initialized = {
+      x :  e.pageX,
+      y :  e.pageY
+    };
+     
+	  var handlers = {
       mousemove : function(e){
         e.preventDefault();
-
-        x=parseInt(e.pageX / bricksize);
-        y=parseInt(e.pageY / bricksize);
-        var new_move=x+"-"+y;
-
-        // Disable brick overflow outside drawspace
-        if (new_move!=last_move && e.pageX < drawspace.width() && e.pageY < drawspace.height() && e.pageX > 0 && e.pageY > 0) {
-          updatePos(x,y);
+        if (options_div.length==0) {
+          x=parseInt(e.pageX / bricksize);
+          y=parseInt(e.pageY / bricksize);
+          var new_move=x+"-"+y;
+          // Disable brick overflow outside drawspace
+          if (new_move!=last_move && e.pageX < drawspace.width() && e.pageY < drawspace.height() && e.pageX > 0 && e.pageY > 0) {
+            updatePos(x,y);
+          }
+          last_move=new_move;
+        } else { // unused
+           options_div.css({
+            left : ( initialized.x + e.pageX - $(document).scrollLeft() ) + 'px',
+            top : ( initialized.y + e.pageY - $(document).scrollTop() ) + 'px',
+            bottom: 'inherit',
+            right: 'inherit',
+           });
         }
-        last_move=new_move;
       },
       mouseup : function(e){
         $(this).off(handlers);   
@@ -125,7 +190,7 @@ $(window).on("load", function (){
   });
 
   /* Gère le click simple (ajout/suppression de briques) sur le drawspace  */
-  drawspace.bind("click", function(e){
+  $(".drawspace").bind("click", function(e){
     if (smartphone == 0) {
       var x,y;
       x=parseInt(e.pageX / bricksize);
@@ -135,28 +200,28 @@ $(window).on("load", function (){
   });
 
   // Remise en place de la scrollbar à sa position iniliale après avoir déclanché l'Off Canvas.
-//   var leftPos,
-//       topPos,
-//       offsetTop,
-//       offsetLeft;
-//   $("#buttonToggle").click(function() {
-//      offsetTop = drawspace.offset().top;
-//      offsetLeft = drawspace.offset().left;
-//     if (!offCanvas.hasClass("is-open") && smartphone == 0) {
-//       leftPos = $('body').scrollLeft();
-//       topPos = $('body').scrollTop();
-//       $("body").animate({scrollLeft: leftPos + 300}, 800);
-//     } else {
-//       $("body").animate({scrollLeft: leftPos - offsetLeft}, 800);
-//       $("body").animate({scrollTop: topPos - offsetTop}, 1);
-//     }
-//   });
-//   // Evenement déclanché lors de la fermeture de l'offCanvas
-//   $(document).on('closed.zf.offcanvas', function() {
-//     $("body").animate({scrollLeft: leftPos - offsetLeft}, 800);
-//     $("body").animate({scrollTop: topPos - offsetTop}, 1);
-//   });
- });
+  var leftPos,
+      topPos,
+      offsetTop,
+      offsetLeft;
+  $("#buttonToggle").click(function() {
+     offsetTop = drawspace.offset().top;
+     offsetLeft = drawspace.offset().left;
+    if (!offCanvas.hasClass("is-open") && smartphone == 0) {
+      leftPos = $('body').scrollLeft();
+      topPos = $('body').scrollTop();
+      $("body").animate({scrollLeft: leftPos + 300}, 800);
+    } else {
+      $("body").animate({scrollLeft: leftPos - offsetLeft}, 800);
+      $("body").animate({scrollTop: topPos - offsetTop}, 1);
+    }
+  });
+  // Evenement déclanché lors de la fermeture de l'offCanvas
+  $(document).on('closed.zf.offcanvas', function() {
+    $("body").animate({scrollLeft: leftPos - offsetLeft}, 800);
+    $("body").animate({scrollTop: topPos - offsetTop}, 1);
+  });
+});
 
 
 function globalInit() {
@@ -173,9 +238,6 @@ function globalInit() {
       event.preventDefault();
     }
   });
-
-  // = 1 si mobile, 0 sinon
-  smartphone = detectDevice();
  
   // Initialisation of the drawspace's brick size
   initBrickSize(bricksize+"px");
@@ -230,7 +292,7 @@ function initBrickSize(size) {
    // });  
 }
 
-/* ------------ Gestion du tactile ------------ */
+
 
 function hammerIt(pDrawspace) {
 
@@ -366,46 +428,4 @@ function updatePos(x, y) {
         brick.set({color: color, x: x, y: y, uid: authData.uid});
     }
   });
-}
-
-
-/* ------------ Gestion de l'affichage de l'off-canvas ------------ */
-
-/* gère la surbrillance d'une couleur active */
-$(".colors .brickMenu").click(function(e) {
-  $(".colors .brickMenu").removeClass("active");
-  $(this).addClass("active");
-  color=$(this).attr('class').replace(/\s*(brick|active)\s*/g, '');
-});
-
-/* empèche l'affichage des couleurs lors du déroulement de la liste si on est en mode "erase" */
-$("#titleDT").click(function() {
-  if (mode == "erase") {
-    $(".ul-drawTools").attr("style", "height: 100px");
-    $(".drawTools-buttons-container").css("height", "100%");
-    $(".color-list").hide();
-  }
-});
-
-$(".button").click(function(e){
-  change_mode($(this).attr("id"))
-});
-
-/* Manage the display mode in the off-canvas menue */
-function change_mode(new_mode) {
-  mode=new_mode;
-  if (mode=="draw" || mode=="eraseAll") {
-    $(".ul-drawTools").attr("style", "height: 200px");
-    $(".drawTools-buttons-container").css("height", "50%");
-    $("#erase"+" :nth-child(1)").removeClass("fa-square").addClass("fa-square-o");
-    $("#draw"+" :nth-child(1)").removeClass("fa-square-o").addClass("fa-square");
-    $(".color-container").show();
-  }
-  else {
-    $(".ul-drawTools").attr("style", "height: 100px");
-    $(".drawTools-buttons-container").css("height", "100%");
-    $("#erase"+" :nth-child(1)").removeClass("fa-square-o").addClass("fa-square");
-    $("#draw"+" :nth-child(1)").removeClass("fa-square").addClass("fa-square-o");
-    $(".color-list").hide();      
-  }
 }
