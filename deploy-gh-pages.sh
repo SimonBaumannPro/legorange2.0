0224b962e73f9df0a3197467e218be5a612e5d4c
SimonBaumannPro <simonbaumann20@gmail.com>
2016-08-24 16:51:01 +0200

    allow only branch basics to publish on gh-page

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -6,4 +6,6 @@ git config user.name "SimonBaumannPro"
 git config user.email "simon.baumann@orange.com"
 git add .
 git commit -m "Deploy to GitHub Pages"
-git push --force --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" master:gh-pages
+if [ "$TRAVIS_BRANCH" == "basics" ]; then
+	git push --force --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" master:gh-pages
+fi
