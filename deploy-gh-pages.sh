f592247b71b2f4b930fd3ccbf8b392cd270cf52f
SimonBaumannPro <simonbaumann20@gmail.com>
2016-11-16 16:07:11 +0100

    mend

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -7,5 +7,5 @@ git config user.email "simon.baumann@orange.com"
 git add .
 git commit -m "Deploy to GitHub Pages"
 if [ "$TRAVIS_BRANCH" == "basics" ]; then
-	git push --force --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" gh-pages   # master:gh-pages
+	git push --force --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" master:gh-pages   # master:gh-pages
 fi
